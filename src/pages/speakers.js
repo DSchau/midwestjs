42b86d63b252ea4543cdd1213f4ff686370ffa60
Dustin Schau <dustinschau@gmail.com>
2018-07-30 10:33:14 -0500

    feat: add speakers

--- src/pages/speakers.js
+++ src/pages/speakers.js
@@ -1,13 +1,62 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { Link as GatsbyLink, graphql } from 'gatsby';
 import styled from 'react-emotion';
+import GatsbyImage from 'gatsby-image';
 
 import Layout from '../components/layout';
 import Subheader from '../components/sub-header';
 
+import { DIMENSIONS } from '../util/dimensions';
+
 const Container = styled.div({
   display: 'flex',
   flexDirection: 'column',
+});
+
+const Content = styled.div({
+  maxWidth: 900,
+  margin: '1rem auto',
+  padding: '1rem',
+});
+
+const Grid = styled.div({
+  display: 'grid',
+  gridTemplateColumns: '100%',
+  ...DIMENSIONS.greaterThan('medium')({
+    gridTemplateColumns: '50% 50%',
+  }),
+  ...DIMENSIONS.greaterThan('large')({
+    gridTemplateColumns: '33% 33% 33%',
+  }),
+});
+
+const Speaker = styled.div({
+  padding: '1rem',
+  margin: '0 auto',
+});
+
+const Name = styled.h2({
+  margin: 0,
+  marginTop: '0.5rem',
+  padding: 0,
+});
+
+const Company = styled.h3({
+  margin: 0,
+  color: '#999',
+});
+
+const Bio = styled.div({
+  fontSize: 14,
+  margin: '1rem 0',
+  lineHeight: 1.5,
+});
+
+const ImageContainer = styled.div({ textAlign: 'center' });
+const Image = styled(GatsbyImage)();
+const Link = styled(GatsbyLink)({
+  textDecoration: 'none',
+  color: 'inherit',
 });
 
 export default function Speakers({ data }) {
@@ -16,6 +65,26 @@ export default function Speakers({ data }) {
     <Layout>
       <Container>
         <Subheader title="Speakers" />
+        <Content>
+          <Grid>
+            {speakers.edges.map(({ node: speaker }) => (
+              <Speaker key={speaker.id}>
+                <Link to={speaker.slug}>
+                  <ImageContainer>
+                    <Image fixed={speaker.avatar.fixed} />
+                  </ImageContainer>
+                  <Name>{speaker.name}</Name>
+                  {speaker.company && <Company>{speaker.company}</Company>}
+                </Link>
+                <Bio
+                  dangerouslySetInnerHTML={{
+                    __html: speaker.bio.childMarkdownRemark.html,
+                  }}
+                />
+              </Speaker>
+            ))}
+          </Grid>
+        </Content>
       </Container>
     </Layout>
   );
@@ -27,13 +96,21 @@ export const pageQuery = graphql`
       edges {
         node {
           id
+          avatar {
+            fixed(height: 250, width: 250) {
+              ...GatsbyContentfulFixed_withWebp
+            }
+          }
           name
           company
           bio {
-            bio
+            childMarkdownRemark {
+              html
+            }
           }
           twitter
           github
+          slug
         }
       }
     }
