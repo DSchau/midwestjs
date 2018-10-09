740b6c4b6aa400ff1fc0c002d457f75eeaf6ccae
Dustin Schau <dustinschau@gmail.com>
2018-10-09 14:43:06 -0500

    chore: remove host

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,6 @@ module.exports = {
       options: {
         spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
         accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
-        host: process.env.GATSBY_CONTENTFUL_HOST || undefined,
       },
     },
     {
