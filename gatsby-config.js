637051968afd4574b748b52816d14aa92e587e25
Dustin Schau <dustinschau@gmail.com>
2018-10-09 13:38:54 -0500

    chore: add logs

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,14 @@ const path = require('path');
 
 require('dotenv').config();
 
+console.log(
+  JSON.stringify({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    bearer_token: process.env.TWITTER_BEARER_TOKEN,
+  })
+);
+
 module.exports = {
   siteMetadata: {
     title: 'Midwest JS',
